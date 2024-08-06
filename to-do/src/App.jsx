34dd1f9f76b092595ac12a07f928cdd935727376
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-08-06 19:28:28 +0500

    first commit

---
+++ to-do/src/App.jsx
@@ -0,0 +1,14 @@
+// Bismillah
+function App() {
+  return (
+  <div className="container"> 
+  <form>
+    <input className="form-control" type="text" placeholder="title" />
+    <button className="btn btn-primary mt-3" style={{ width:"150px" }} >Add todo</button>
+  </form>
+  </div>
+
+)
+}
+
+export default App
